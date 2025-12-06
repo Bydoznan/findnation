f73e91a8c3f1e6fa2d557ffc97aa84d09a2895ef
CandyyPL <aplay58.yt@gmail.com>
2025-12-06 21:00:09 +0100

    frontend initial commit

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
+import tailwindcss from "eslint-plugin-tailwindcss";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  ...tailwindcss.configs["flat/recommended"],
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
